bc8049acdaec7d94220db2aef5cbd48ff2d56ee5
wap4444 <wap4444@mail.ru>
2017-07-24 01:39:37 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -21,6 +21,25 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
+        
+        function checkConnection() {
+    var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+ 
+    alert('Connection type: ' + states[networkState]);
+}
+ 
+checkConnection();
+        
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
