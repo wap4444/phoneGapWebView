6d3543ac939d2327dda79e7167ef677ed44bba38
wap4444 <wap4444@mail.ru>
2018-01-15 23:14:10 +0600

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -21,7 +21,7 @@ var app = {
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        
+window.location.href = 'https://google.com';
         function checkConnection() {
     var networkState = navigator.connection.type;
  
